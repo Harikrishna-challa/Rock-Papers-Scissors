159b21964749628e7e8cfc840fa421c9a94d71fe
Harikrishna-Challa <harichvk18@gmail.com>
2025-02-07 13:33:34 +0530

    Re-deploying to fix 404 error

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
+// This event listener ensures that the script executes only after the DOM content has been fully loaded.
 document.addEventListener('DOMContentLoaded',function(){
+     // Selecting all elements with the class 'choice'
 const choices =document.querySelectorAll(".choice");
+// Selecting the element with the id 'msg'
 const msg =document.getElementById("msg");
-
+// Selecting elements for displaying user and computer scores
 const userScorePara=document.querySelector("#user-score");
 const compScorePara=document.querySelector("#comp-score");
+    // Initializing user and computer scores
  let userScore=0;
  let compScore=0;
+ // Function to generate computer choice (Rock, Paper, or Scissors)
 const genCompChoice = ()=>
 {
     const options=["Rock","Paper","Scissors"];
     const randIdx=Math.floor(Math.random()*3);
     return options[randIdx];
 };
+// Function to handle a draw game
 const drawGame = () =>{
     msg.innerText="Game was Draw.Play again";
     msg.style.backgroundColor="#081b31";
 };
+ // Function to determine the winner and update scores
 const showWinner=(userWin,userChoice,CompChoice) =>{
     if(userWin){
         userScore++;
@@ -39,6 +46,7 @@ const playGame = (userChoice) => {
       drawGame();
   } else {
       let userWin = false;
+        // Check winning conditions
       if (userChoice === "Rock") {
           // if user chooses rock
           if(compChoice === "Scissors"){
@@ -55,9 +63,11 @@ const playGame = (userChoice) => {
           userWin=true; // Scissors beats Paper
       }
     }
+    // Display the winner and update scores
       showWinner(userWin, userChoice, compChoice);
   }
 };
+// Adding event listeners to each choice
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
         const userChoice = choice.getAttribute("id");
